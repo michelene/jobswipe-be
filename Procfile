5b0d18407f59f3318d37147c7ba3cbeefabe5bd4
Michelene Chon <michelenechon@gmail.com>
2020-03-10 12:29:23 -0700

    Add Heroku-required files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn jobswipe-be --log-file -
--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6vea5$znl-c4ae1j8gf8ys-cy(nyqt9bgwdoy^-3^++ezc5hhb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jobswipe-be.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
